8a87b56a6c2f7ac55b80f36f7739ebccb0298eb4
novon229 <novon229@gmail.com>
2015-12-08 10:04:05 -0500

    Removed solution reset option Removed unnecessary info from cube_state
    Code does not reevaluate when its been changed so mashing e is bad

--- Project1/Project1/Rubik.cpp
+++ Project1/Project1/Rubik.cpp
@@ -1004,113 +1004,6 @@ int colour_interp(double *current) {
 	else return 7;
 }
 int** cube_state() {
-	std::vector<int> cube_position_colour;
-	int current;
-	/*
-	
-	//---------------Top face
-	
-
-	//------------------front face
-
-	//top front row
-	arr[0][0] = colour_interp(frontTopLeft->getColours(5));
-	arr[0][1] = colour_interp(frontTopMiddle->getColours(5));
-	arr[0][2] = colour_interp(frontTopRight->getColours(5));
-	//middle front row
-	arr[0][3] = colour_interp(frontMiddleLeft->getColours(5));
-	arr[0][4] = colour_interp(frontMiddleMiddle->getColours(5));
-	arr[0][5] = colour_interp(frontMiddleRight->getColours(5));
-	//bottom front row
-	arr[0][6] = colour_interp(frontBottomLeft->getColours(5));
-	arr[0][7] = colour_interp(frontBottomMiddle->getColours(5));
-	arr[0][8] = colour_interp(frontBottomRight->getColours(5));
-
-
-	//------ right face
-	arr[1][0] = colour_interp(frontTopRight->getColours(2));
-	arr[1][1] = colour_interp(middleTopRight->getColours(2));
-	arr[1][2] = colour_interp(backTopRight->getColours(2));
-
-	//middle back row
-	arr[1][3] = colour_interp(frontMiddleRight->getColours(2));
-	arr[1][4] = colour_interp(middleMiddleRight->getColours(2));
-	arr[1][5] = colour_interp(backMiddleRight->getColours(2));
-
-	//bottom back row
-	arr[1][6] = colour_interp(frontBottomRight->getColours(2));
-	arr[1][7] = colour_interp(middleBottomRight->getColours(2));
-	arr[1][8] = colour_interp(backBottomRight->getColours(2));
-
-	
-	//top of cube
-	//---------------------------
-	arr[2][0] = colour_interp(frontTopLeft->getColours(1));
-	arr[2][1] = colour_interp(frontTopMiddle->getColours(1));
-	arr[2][2] = colour_interp(frontTopRight->getColours(1));
-	//middle top row
-	arr[2][3] = colour_interp(middleTopLeft->getColours(1));
-	arr[2][4] = colour_interp(middleTopMiddle->getColours(1));
-	arr[2][5] = colour_interp(middleTopRight->getColours(1));
-
-	//back top row
-	arr[2][6] = colour_interp(backTopLeft->getColours(1));
-	arr[2][7] = colour_interp(backTopMiddle->getColours(1));
-	arr[2][8] = colour_interp(backTopRight->getColours(1));
-
-
-	//---back face
-	arr[3][0] = colour_interp(backTopLeft->getColours(4));
-	arr[3][1] = colour_interp(backTopMiddle->getColours(4));
-	arr[3][2] = colour_interp(backTopRight->getColours(4));
-
-	//middle back row
-	arr[3][3] = colour_interp(backMiddleLeft->getColours(4));
-	arr[3][4] = colour_interp(backMiddleMiddle->getColours(4));
-	arr[3][5] = colour_interp(backMiddleRight->getColours(4));
-
-	//bottom back row
-	arr[3][6] = colour_interp(backBottomLeft->getColours(4));
-	arr[3][7] = colour_interp(backBottomMiddle->getColours(4));
-	arr[3][8] = colour_interp(backBottomRight->getColours(4));
-
-
-	//---------left face of the cube
-	//top back row
-	arr[4][0] = colour_interp(frontTopLeft->getColours(0));
-	arr[4][1] = colour_interp(middleTopLeft->getColours(0));
-	arr[4][2] = colour_interp(backTopLeft->getColours(0));
-
-	//middle back row
-	arr[4][3] = colour_interp(frontMiddleLeft->getColours(0));
-	arr[4][4] = colour_interp(middleMiddleLeft->getColours(0));
-	arr[4][5] = colour_interp(backMiddleLeft->getColours(0));
-
-	//bottom back row
-	arr[4][6] = colour_interp(frontBottomLeft->getColours(0));
-	arr[4][7] = colour_interp(middleBottomLeft->getColours(0));
-	arr[4][8] = colour_interp(backBottomLeft->getColours(0));
-	
-	//----------------------------------
-
-
-	//--------bottom of the cube
-
-	//bottom front row
-	arr[5][0] = colour_interp(frontBottomLeft->getColours(3));
-	arr[5][1] = colour_interp(frontBottomMiddle->getColours(3));
-	arr[5][2] = colour_interp(frontBottomRight->getColours(3));
-
-	//bottom middle row
-	arr[5][3] = colour_interp(middleBottomLeft->getColours(3));
-	arr[5][4] = colour_interp(middleBottomMiddle->getColours(3));
-	arr[5][5] = colour_interp(middleBottomRight->getColours(3));
-
-	//bottom front row
-	arr[5][6] = colour_interp(backBottomLeft->getColours(3));
-	arr[5][7] = colour_interp(backBottomMiddle->getColours(3));
-	arr[5][8] = colour_interp(backBottomRight->getColours(3));
-	*/
 	return arr;
 
 }
@@ -1207,12 +1100,6 @@ display(void)
 				animation_tic_count = 0;
 			}
 		}
-		else {
-			//when the entire solution has been done reset the solution boolean and string index
-			//if keyboard based scrambling is implemented this will allow the program to resolve the cube
-			current_string_index = 0;
-			solution_boolean = 0;
-		}
 	}
 	//draw the current boxes into the window
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -1253,10 +1140,7 @@ display(void)
 	c27.drawBox();
 
 	// Attempt to draw status text
-	if (wasSolutionAttempted) {
-
-	}
-	else {
+	if (wasSolutionAttempted==false) {
 		displayText(0, 440 - 10, 122, 122, 122, "Press 'E' to simulate the solution.");
 	}
 
